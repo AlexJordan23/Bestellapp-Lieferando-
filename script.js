0c03150c1f2b0216c8e83daf75c4f3ee976e26e0
Alexander Winner <alexanderwinner06@gmail.com>
2026-05-26 11:12:45 +0200

    Dateien in Lieferando geschoben

---
+++ script.js
@@ -0,0 +1,49 @@
+let items = [
+    {
+        "Kategorie" : "Burger & Sandwitches",
+        "Gericht" : "Veggie mushroom black burger",
+        "Zutaten" : "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+        "Preis" : "16,90€",
+    },
+    {
+        "Kategorie" : "Pizza",
+        "Gericht" : "Pizza Margherita",
+        "Zutaten" : "Tomato Souce, Mozzarella",
+        "Preis" : "11,90€",
+    },
+    {
+        "Kategorie" : "Salad",
+        "Gericht" : "Warm beef arugula salad",
+        "Zutaten" : "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic vinegar dressing",
+        "Preis" : "16,90€",
+    },
+    {
+        "Kategorie" : "Salad",
+        "Gericht" : "Warm beef arugula salad",
+        "Zutaten" : "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic vinegar dressing",
+        "Preis" : "16,90€",
+    }
+]
+
+function getItemTemplate(indexItem){
+    return `
+    <div class="item_card">
+        <h2>${items[indexItem].Gericht}</h2>
+    </div>
+
+    <div class="price">
+        <p class="price">${items[indexItem].Preis}</p>
+        <p class="price"></p>
+    </div>         
+    `
+}
+
+function renderItem() {
+  let contentRef = document.getElementById('content');
+  contentRef.innerHTML = "";
+
+  for (let indexItem = 0; indexItem < items.length; indexItem++) {
+    contentRef.innerHTML += getItemTemplate(indexItem);
+    
+  }
+}
