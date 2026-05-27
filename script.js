209eec92f6e0732232e60161878db9271ac23c7b
Alexander Winner <alexanderwinner06@gmail.com>
2026-05-27 17:30:37 +0200

    ckickenburger.png hinzugefügt

--- script.js
+++ script.js
@@ -20,15 +20,15 @@ let items = [
         "Gericht": "Beef red Burger",
         "Zudaten": "Beef, Cheese, Tomatoes, Lettuce, Onion",
         "Preis": "14,90€",
-        "Bild": "burger.png",
+        "Bild": "redburger.png",
     },
 
     {
         "Kategorie": "Burger",
-        "Gericht": "All meat burger",
-        "Zudaten": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souce",
+        "Gericht": "Blg chicken burger",
+        "Zudaten": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
         "Preis": "15,90€",
-        "Bild": "burger.png",
+        "Bild": "",
     },
     
     
